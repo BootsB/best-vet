9d166f6ced0eca371f0e8e1f4325e4ba0aca1b6c
CristianManolache <mlcristian@outlook.com>
2024-03-02 12:46:12 +0000

    added review model with references

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :pet_profile
+  has_one :review
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_02_124151) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_02_124436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_124151) do
     t.index ["user_id"], name: "index_pet_profiles_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "appointment_id", null: false
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["appointment_id"], name: "index_reviews_on_appointment_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_124151) do
   add_foreign_key "appointments", "pet_profiles"
   add_foreign_key "appointments", "users"
   add_foreign_key "pet_profiles", "users"
+  add_foreign_key "reviews", "appointments"
 end
