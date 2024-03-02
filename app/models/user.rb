656be90cb10c6b5c2b9166f51246fd760c98f41a
CristianManolache <mlcristian@outlook.com>
2024-03-02 13:01:49 +0000

    fix some missing content

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pet_profiles
-  has_many :appointments
+  has_many :appointments, through: :pet_profiles
   has_many :posts
   has_many :replies
+  has_many :reviews, through: :appointments
+
+  # Validations
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_02_125016) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_02_130005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_125016) do
     t.string "city"
     t.text "description"
     t.integer "membership"
+    t.string "phone_number"
+    t.string "state"
+    t.string "zip_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
