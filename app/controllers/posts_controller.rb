9121770b0d9dedeb312586ad437b5a48d2d0f9b5
boots britton <boots.britton@yahoo.co.uk>
2024-03-05 20:26:12 +0000

    new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,29 @@ class PostsController < ApplicationController
     @reply = Reply.new
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def set_post
     @post = Post.find(params[:id])
   end
 
+  def post_params
+    params.require(:post).permit(:title, :category_id, :content)
+  end
 
 end
