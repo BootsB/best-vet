d98e896566f94affb88ab4b36da547f73857484c
CristianManolache <mlcristian@outlook.com>
2024-03-09 16:34:37 +0000

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_09_162142) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_09_163337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,10 +107,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_09_162142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "vet"
-    t.bigint "user_profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_profile_id"], name: "index_users_on_user_profile_id"
   end
 
   add_foreign_key "appointments", "pet_profiles"
@@ -121,5 +119,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_09_162142) do
   add_foreign_key "replies", "posts"
   add_foreign_key "replies", "users"
   add_foreign_key "reviews", "appointments"
-  add_foreign_key "users", "user_profiles"
 end
