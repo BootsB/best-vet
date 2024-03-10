9d62fafe7c92abd5ffe403bc563ce00ffd3fe0b7
boots britton <boots.britton@yahoo.co.uk>
2024-03-10 11:16:18 +0000

    new pet form

--- app/controllers/pet_profiles_controller.rb
+++ app/controllers/pet_profiles_controller.rb
@@ -2,4 +2,22 @@ class PetProfilesController < ApplicationController
   def show
     @pet = PetProfile.find(params[:id])
   end
+
+  def new
+    @pet = PetProfile.new
+  end
+                  
+  def create
+    @pet = PetProfile.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_profile_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+  end
+
+
 end
