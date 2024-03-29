9043648f242e18eeee572e20ed051fe1f257984e
CristianManolache <mlcristian@outlook.com>
2024-03-29 18:38:48 +0000

    Fix models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :pet_profile
-  has_one :review
   has_one :user_profile, through: :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   delegate :first_name, :last_name, :address, :description, :membership, :location, :city, :phone_number, :state, :zip_code, :licence_number, to: :user_profile, allow_nil: true
 
